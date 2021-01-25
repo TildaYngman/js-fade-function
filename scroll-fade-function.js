4087ea909aaefdce466fc8d3390f25cbea66497d
Tilda Yngman <73299046+TildaYngman@users.noreply.github.com>
2021-01-25 20:09:08 +0100

    Update scroll-fade-function.js

--- scroll-fade-function.js
+++ scroll-fade-function.js
@@ -1,17 +1,17 @@
 
-/*$(window).on("load",function() {
-		  $(window).scroll(function() {
-			var windowBottom = $(this).scrollTop() + $(this).innerHeight();
-			$(".fade").each(function() {
-			  var objectBottom = $(this).offset().top + $(this).outerHeight();
+$(window).on("load",function() {
+	$(window).scroll(function() {
+	var windowBottom = $(this).scrollTop() + $(this).innerHeight();
+	$(".fade").each(function() {
+		var objectBottom = $(this).offset().top + $(this).outerHeight();
 			  
-			  if (objectBottom < windowBottom) {
-				if ($(this).css("opacity")==0) {$(this).fadeTo(300,1);}
-			  } else {
-				if ($(this).css("opacity")==1) {$(this).fadeTo(300,0);}
-			  }
-			});
+		if (objectBottom < windowBottom) {
+			if ($(this).css("opacity")==0) {$(this).fadeTo(300,1);}
+		} else {
+			if ($(this).css("opacity")==1) {$(this).fadeTo(300,0);}
+			}
+		});
 		  }).scroll();
-		}); */
+		});
 				
-				//För att använda denna på andra object så behöver de ha en class .fade.
+				//right now the class on what you want to fade is .fade, change to your liking.
