93dc6e3a7f72c9804ea6c4720326e4850674660c
Tilda Yngman <73299046+TildaYngman@users.noreply.github.com>
2021-01-25 20:04:17 +0100

    Add files via upload

---
+++ scroll-fade-function.js
@@ -0,0 +1,17 @@
+
+/*$(window).on("load",function() {
+		  $(window).scroll(function() {
+			var windowBottom = $(this).scrollTop() + $(this).innerHeight();
+			$(".fade").each(function() {
+			  var objectBottom = $(this).offset().top + $(this).outerHeight();
+			  
+			  if (objectBottom < windowBottom) {
+				if ($(this).css("opacity")==0) {$(this).fadeTo(300,1);}
+			  } else {
+				if ($(this).css("opacity")==1) {$(this).fadeTo(300,0);}
+			  }
+			});
+		  }).scroll();
+		}); */
+				
+				//För att använda denna på andra object så behöver de ha en class .fade.
